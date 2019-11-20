4c5b76ce2871b6176c7585373ddc90187cda3583
jane-jianfangzhang <57983058+jane-jianfangzhang@users.noreply.github.com>
2019-11-20 19:56:48 +1100

    buythisspace testing v1
    buythisspace testing v1

---
+++ test_cases.py
@@ -0,0 +1,29 @@
+import pytest
+import requests
+import json
+
+
+class TestCases:
+
+    def test_get_homepage(self):
+
+        url = "https://buythisspace.com.au/"
+        headers = {
+                   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                 'Chrome/78.0.3904.97 Safari/537.36'
+                   }
+        response = requests.get(url, headers=headers)
+        # response = json.loads(response.content)
+        # print(response)
+
+        if response.status_code != requests.codes.ok:
+            raise AssertionError("Common validation is fail! the expected response code is %s, but the actual "
+                                 "response status code result is %s"
+                                 % (int(requests.codes.ok), response.status_code))
+        else:
+            print("The result is %s, " % response)
+
+
+# if __name__ == "__main__":
+#     homepage = TestCases.test_get_homepage()
+#     print(homepage)
---
+++ test_runner.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+root_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+
+
+def run(command):
+    """
+    To run all the test cases
+    :return:
+    """
+    subprocess.run(command, shell=True)
+
+
+if __name__ == "__main__":
+
+    test_command = "pytest -s " + os.path.join(root_path, "test_cases.py::TestCases::test_get_homepage") + \
+                   " --html=" + os.path.join(root_path, "report_output", "buy_this_space_testing_report.html")
+    run(test_command)
